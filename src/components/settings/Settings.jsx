43b2d10d0cf08f1d7de9a89413ffec6eed4e3eb4
MrBatata <mn.martinez@live.com.ar>
2023-05-15 23:31:42 -0300

    latest

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import useLocalStorage from "../../hooks/useLocalStorage";
 
 const defaultConfig = {
-  theme: "dark"
+  lang: 'es',
+  theme: 'dark'
 };
 
 export default function Settings({ toggleDark }) {
